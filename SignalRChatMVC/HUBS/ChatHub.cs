993f4e99261670b227f8e399c04c5e66df1fe25e
lduc292004 <lduc292004@gmail.com>
2025-10-20 01:12:01 +0600

    Add project files.

---
+++ SignalRChatMVC/HUBS/ChatHub.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace SignalRChatMVC.Hubs
+{
+    public class ChatHub : Hub
+    {
+        // Danh sách người dùng đang online
+        private static ConcurrentDictionary<string, string> Users = new();
+
+        public override async Task OnConnectedAsync()
+        {
+            var userName = Context.GetHttpContext()?.Request.Query["username"];
+            if (!string.IsNullOrEmpty(userName))
+            {
+                Users[Context.ConnectionId] = userName!;
+                await Clients.All.SendAsync("UserJoined", userName);
+                await Clients.All.SendAsync("UpdateUserList", Users.Values);
+            }
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            if (Users.TryRemove(Context.ConnectionId, out var user))
+            {
+                await Clients.All.SendAsync("UserLeft", user);
+                await Clients.All.SendAsync("UpdateUserList", Users.Values);
+            }
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            var time = DateTime.Now.ToString("HH:mm:ss");
+            await Clients.All.SendAsync("ReceiveMessage", user, message, time);
+        }
+
+        public async Task SendPrivateMessage(string toUser, string fromUser, string message)
+        {
+            var connectionId = Users.FirstOrDefault(u => u.Value == toUser).Key;
+            if (connectionId != null)
+            {
+                var time = DateTime.Now.ToString("HH:mm:ss");
+                await Clients.Client(connectionId).SendAsync("ReceivePrivateMessage", fromUser, message, time);
+            }
+        }
+    }
+}
---
+++ SignalRChatMVC/Models/ChatMessagecs.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalRChatMVC.Models
+{
+    public class ChatMessage
+    {
+        public string User { get; set; }
+        public string Message { get; set; }
+        public string Avatar { get; set; }
+    }
+}
